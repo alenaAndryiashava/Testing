1935bbd2750041a4152059cae52dddbeb1b44ce1
Alena Andryiashava <alena.andryiashava@gmail.com>
2023-01-16 12:20:29 +0100

    phonebook Project

---
+++ phonebookProject/src/main/java/pages/AccountPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AccountPage extends PageBase{
+
+    public AccountPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
