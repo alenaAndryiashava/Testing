a4d7e33dd80129cf67cdbca7b325f0e21c090b0b
Alena Andryiashava <alena.andryiashava@gmail.com>
2023-04-05 14:32:55 +0200

    TestBase and pom.xml

---
+++ taskProject/src/main/java/tests/TestBase.java
@@ -0,0 +1,41 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+public class TestBase {
+    protected WebDriver driver;
+    protected Faker faker = new Faker();
+    String urlBank = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login";
+    String urlGlobalSqa = "https://www.globalsqa.com/samplepagetest/";
+
+    @BeforeMethod
+    public void setUp() {
+//        driver = new ChromeDriver();
+        //driver = new FirefoxDriver();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
+//        driver = new ChromeDriver(options);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        driver.get(urlGlobalSqa);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+}
